656f3ac9a27db23646f300776f10782b7a143339
csyntax <167671289+Csyntax@users.noreply.github.com>
2024-06-10 11:30:05 +0100

    Initial commit: Add Hello World application

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+//Console.WriteLine("Hello, World!");
